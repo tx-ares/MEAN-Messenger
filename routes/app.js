20a03f5a4ab9c071a959d81ca67493e693b8b620
Adan <twinadanz@gmail.com>
2018-03-15 21:42:36 -0500

    adding node route

--- routes/app.js
+++ routes/app.js
@@ -5,4 +5,8 @@ router.get('/', function (req, res, next) {
     res.render('index');
 });
 
+router.get('/message', function (req, res, next) {
+    res.render('node', {message: "Yoyo!"}); // With each of the routes, we can pass data as objects as our 2nd argument. 
+});
+
 module.exports = router;
