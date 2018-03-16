905f675a84e6c1ec4bf8268d03df56b1fed933eb
Adan <twinadanz@gmail.com>
2018-03-15 21:57:00 -0500

    add post message route

--- routes/app.js
+++ routes/app.js
@@ -5,8 +5,13 @@ router.get('/', function (req, res, next) {
     res.render('index');
 });
 
-router.get('/message', function (req, res, next) {
-    res.render('node', {message: "Yoyo!"}); // With each of the routes, we can pass data as objects as our 2nd argument. 
+router.get('/message/:msg', function (req, res, next) { // A GET route will consume some data from a source after making a request.
+    res.render('node', {message: req.params.msg}); // With each of the routes, we can pass data as objects as our 2nd argument.  We can access the URL variable set above by accessing the params followed by the name of the URL variable.
+});
+
+router.post('/message', function (req, res, next) {
+    var message = req.body.message; // By accessing our request, we can access the body which contains the bulk of our data and we will assume there is an attribute named 'message'
+    res.redirect('/message/' + message); //
 });
 
 module.exports = router;
