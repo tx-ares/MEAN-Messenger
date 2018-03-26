8fbfa0e83d7fdb9f539b47b6c8b7d043704844e4
Adan <twinadanz@gmail.com>
2018-03-26 00:38:55 -0500

    added user & model schemas  , installed package

---
+++ models/message.js
@@ -0,0 +1,10 @@
+//Here will contain message model
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema; // A Schema is a blueprint for our model. The Schema constructor will allow us to create new instances of a message schema. It will contain all the keys and values that will be needed for a message object.
+
+var schema = new Schema({
+  content: {type: String, required: true},
+  user: {type: Schema.Types.ObjectId}
+});
+
+module.exports = mongoose.model('Message' , schema); //This will export a model called Message by passing in the name of the Model and its schema then will generate a collection automatically if it is the first of its kind.
---
+++ models/user.js
@@ -0,0 +1,13 @@
+//Here will contain user model
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema; // A Schema is a blueprint for our model. The Schema constructor will allow us to create new instances of a message schema. It will contain all the keys and values that will be needed for a message object.
+
+var schema = new Schema({
+  firstName: {type: String, required: true}, //The keys here for 'type' and 'required' are setting rules for this model.  They must be strings, and it cannot be blank.
+  lastName: {type: String, required: true},
+  password: {type: String, required: true},
+  email: {type: String, required: true, unique: true}, //This key has an additional 'unique' key.  It means that no other emails can match its string value.  The 'unique' key's functionality is added by additional package called 'unique-validator'
+  messages:
+});
+
+module.exports = mongoose.model('User' , schema); //This will export a model called Message and will generate a collection automatically if it is the first of its kind.
