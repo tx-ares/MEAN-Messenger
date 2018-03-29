1c848d2328e43edd54f2b871eb85e2ec086a23d7
Adan <twinadanz@gmail.com>
2018-03-29 17:16:11 -0500

    Cleaning up files , setting up for angular

--- routes/app.js
+++ routes/app.js
@@ -8,41 +8,10 @@
 
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user'); //Import the User model
 
 router.get('/', function (req, res, next) {
-    User.findOne({} , function(err, doc) { // An async mongoose method called .findOne() can return a single instance of a object in our db.
-      // It is important to note: As this is an async operation, the data will ONLY be available in this callback function.  Trying to call it outside of it will result in error because the request likely isn't finished yet!
-      if (err) {
-        return res.send('Error!');
-      }
-      res.render('node' , {email: doc.email});
+      res.render('index'); // Now setting up Angular 5 to become our index,  it being a SPA , it will only use this file to display our views and change / re-render using other views.
     });
-});
-
-// router.get('/message', function (req, res, next) { // A GET route will consume some data from a source after making a request.
-//     res.render('node', {message: req.params.msg}); // With each of the routes, we can pass data as objects as our 2nd argument.  We can access the URL variable set above by accessing the params followed by the name of the URL variable.
-// });
-
-router.post('/', function (req, res, next) {
-    // var message = req.body.message; // By accessing our request, we can access the body which contains the bulk of our data and we will assume there is an attribute named 'message'
-    var email = req.body.email;
-    var user = new User({
-      firstName: 'Genji',
-      lastName: 'Shimada',
-      password: 'ryugekkan',
-      email: email // 'email' Variable declared above.
-    });
-    user.save(function(err, result) { //This tells mongoose to save this object in our database.
-      if(err) { // Error handler
-        alert(err);
-      }
-
-      else {
-        console.log("Message object saved: " + result);
-      }
-    });
-    res.redirect('/');
 });
 
 module.exports = router;
