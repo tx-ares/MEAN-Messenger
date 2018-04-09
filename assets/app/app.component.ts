85e47b7ac4ab8e0a5ecf67b33adab00cb19e6375
Adan <twinadanz@gmail.com>
2018-04-09 16:34:32 -0500

    created test message & styles

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -2,8 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({ //This is creating a component and selecting the template it will use in the templateUrl key.
     selector: 'my-app',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    styles: [`
+        .author {
+          display: inline-block;
+          font-style: italic;
+          font-size: 12px;
+          width: 80%;
+        }
+        .config {
+          display: inline-block;
+          text-align: right;
+          font-size: 12px;
+          width: 19%;
+        }
+      `] // Angular also allows us to specically target elements within a component for styling.  We can pass it in as normal CSS here.
 })
 export class AppComponent {
-  content = 'Mic-a-rino Check-a-rino';
+  message = {
+    content: "Yo yo yo it's a message",
+    author: "Adan"
+  }
 }
