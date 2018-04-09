50d8365a472431a80050416a7a1c2c612aa92f49
Adan <twinadanz@gmail.com>
2018-04-09 18:06:35 -0500

    Added optional modifers for message model

--- assets/app/message.model.ts
+++ assets/app/message.model.ts
@@ -1,8 +1,8 @@
 export class Message { // Note that this is a TypeScript object.  Syntax is slightly different... ;'s instead of commas seperating property / value pairs.
     content: string;
     username: string;
-    messageId: string;
-    userId: string;
+    messageId?: string;
+    userId?: string;
 
     constructor(content: string, username: string, messageId: string, userId: string) {
         this.content = content;
