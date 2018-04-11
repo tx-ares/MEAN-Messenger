bea697073487ac606ce70654e0d4f76d2af9af95
Adan <twinadanz@gmail.com>
2018-04-11 00:22:41 -0500

    Created message input into app-message element

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 
+import { Message } from "./messages/message.model";
+
 @Component({ //This is creating a component and selecting the template it will use in the templateUrl key.
     selector: 'my-app',
     templateUrl: './app.component.html',
     styles: [] // Angular also allows us to specically target elements within a component for styling.  We can pass it in as normal CSS here.
 })
 export class AppComponent {
-    message = {
-        content: "Yo yo yo it's a message",
-        author: "Adan"
-    }
+    message = Message = new Message ('A Constructor made this message!', 'Adan';)
 }
--- assets/app/messages/message.component.ts
+++ assets/app/messages/message.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
+
+import { Message } from "./message.model";
 
 @Component({
     selector: 'app-message', // Pass in a CSS Selector ex)  'something' --> <something>
@@ -19,5 +21,5 @@ import { Component } from "@angular/core";
       `] // Angular also allows us to specically target elements within a component for styling.  We can pass it in as normal CSS here.
 })
 export class MessageComponent {
-
+    @Input('myMessageAliasName') message: Message; //By using 'property binding' we can pass in the message property.  Also, by adding '@Input()' to the property name, it can be made assignable from an outside source. i.e. An input field.
 }
