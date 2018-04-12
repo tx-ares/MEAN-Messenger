3aa7ecba5bca56a30852e2bc96056a447828d114
Adan <twinadanz@gmail.com>
2018-04-11 23:29:34 -0500

    Creat input component

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from "./app.component";
+import { AppComponent } from './app.component';
 import { MessageComponent } from './messages/message.component';
 import { MessageListComponent } from './messages/message-list.component';
+import { MessageInputComponent } from './messages/message-input.component';
 
 @NgModule({ // This is a typescript Decorator that attaches additional info to a class.
     declarations: [
         AppComponent,
         MessageComponent,
-        MessageListComponent
+        MessageListComponent,
+        MessageInputComponent
     ],
     imports: [BrowserModule, FormsModule],
     bootstrap: [AppComponent]
