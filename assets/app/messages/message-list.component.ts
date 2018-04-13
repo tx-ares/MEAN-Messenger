5ec31367f87f3373acb723d1383b2e4f53aff5d6
Adan <twinadanz@gmail.com>
2018-04-13 10:34:02 -0500

    Removed dummy messages , fixed getMessages method

--- assets/app/messages/message-list.component.ts
+++ assets/app/messages/message-list.component.ts
@@ -1,5 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
+import { MessageService } from "./message.service";
 import { Message } from "./message.model";
 
 @Component ({ //This is a decorator.  A Component decorator.  And it's not for decoration.  Like, it doesn't make my code any prettier.  I mean, it could.... with the right Atom plugin... but.. I don't have it.. so it's ugly.  It's an ugly decorator.
@@ -11,14 +12,19 @@ import { Message } from "./message.model";
                 (editClicked)="message.content = $event"
                 *ngFor="let message of messages">
             </app-message>
-        </div>`
+        </div>`,
     // We can use the 'structural directive' *ngFor to create a loop that will iterate through all of our message array.
     // This is the entry point for our 'message' component as seen as the 'selector' in message.component.ts   It also listens for an event called 'editClicked' which I defined in message.component.ts as a @Output() event.
+    providers: [MessageService]
 })
-export class MessageListComponent {
-    messages: Message[] = [
-        new Message ('A Constructor made this message!', 'Adan'),
-        new Message ('A 2nd message in our message array! Look at dat!', 'Adan'),
-        new Message ('A 3rd message in our message array! Look at dat!', 'Adan')
-    ];
+export class MessageListComponent implements OnInit { //'implements OnInit' is a ng Life Cycle method which can contain code that will run when this component is initialized. Below, we can define that set of instructions on init in the 'ngOnInit()' function.
+    messages: Message[] = [];
+
+    constructor(private messageService: MessageService) {}
+
+    ngOnInit() {
+        this.messages = this.messageService.getMessages();
+    }
 }
+
+//Arrays and Objects in JavaScript are 'reference types'
