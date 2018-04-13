0e8b2259aaf9ffeb0ae7144e7134595b9d126555
Adan <twinadanz@gmail.com>
2018-04-13 10:17:03 -0500

    Created message service

---
+++ assets/app/messages/message.service.ts
@@ -0,0 +1,17 @@
+import { Message } from "./message.model";
+
+export class MessageService {
+    private messages: Message[] = []; //By adding 'private' to this to make it non-accessible from the outside.
+
+    addMessage(message: Message) { //This is our main function of the 'MessageService'
+        this.messages.push(message);
+    }
+
+    getMessage() {
+        return this.messages;
+    }
+
+    deleteMessage(message: Message) {
+        this.messages.splice(this.messages.indexOf(message) , 1); //We can use the JavaScript method .splice to remove an index from our array.  In this case, to remove a message from the messages array.
+    }
+}
