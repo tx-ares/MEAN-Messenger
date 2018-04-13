dba694222ad25ed915c1465468de3ddffdf293e9
Adan <twinadanz@gmail.com>
2018-04-13 14:09:25 -0500

    added active state styling for links

--- assets/app/header.component.ts
+++ assets/app/header.component.ts
@@ -6,8 +6,8 @@
         <header class="row">
             <nav class="col-md-8 col-md-offset-2">
                 <ul class="nav nav-pills">
-                    <li><a [routerLink]="['/messages']">Messenger</a></li>
-                    <li><a [routerLink]="['/auth']">Authentication</a></li>
+                    <li routerLinkActive="active"><a [routerLink]="['/messages']">Messenger</a></li>
+                    <li routerLinkActive="active"><a [routerLink]="['/auth']">Authentication</a></li>
                 </ul>
             </nav>
         </header>
