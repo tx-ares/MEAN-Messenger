f29941229c86fb6e84140a91a9e9b55d7214d0fb
Adan <twinadanz@gmail.com>
2018-04-13 14:55:31 -0500

    Created dynamic form

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, Title } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MessageComponent } from './messages/message.component';
@@ -29,7 +29,7 @@ import { router } from "./app.router";
         SignupComponent,
         SigninComponent
     ],
-    imports: [BrowserModule, FormsModule, router],
+    imports: [BrowserModule, FormsModule, router, ReactiveFormsModule],
     providers: [Title],
     bootstrap: [AppComponent]
 })
--- assets/app/auth/signup.component.ts
+++ assets/app/auth/signup.component.ts
@@ -1,9 +1,26 @@
 import { Component } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
     selector: 'app-signup',
-    template: `./signup.component.html`
+    templateUrl: './signup.component.html'
 })
 export class SignupComponent {
+    myForm : FormGroup; //There exists a FormBuilder tool with Angular... Look into more later..
 
+    onSubmit() {
+        console.log(this.myForm);
+    }
+
+    ngOnInit() { //This is how Angular can programatically create a form.  Complete with validation!
+        this.myForm = new FormGroup({
+            firstName: new FormControl(null, Validators.required),
+            lastName: new FormControl(null, Validators.required),
+            email: new FormControl(null, [
+                Validators.required,
+                Validators.pattern('/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/')
+            ]),
+            password: new FormControl(null, Validators.required)
+        });
+    }
 }
