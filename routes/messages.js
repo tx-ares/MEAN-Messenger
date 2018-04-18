a1a9bf52b8330b6c997ff46fc784c55219917117
Adan <twinadanz@gmail.com>
2018-04-17 22:21:53 -0500

    add POST route logic for messages

--- routes/messages.js
+++ routes/messages.js
@@ -4,8 +4,22 @@ var router = express.Router();
 
 var Message = require('../models/message');
 
-router.get('/', function(req, res, next) {
-    res.render('index');
+router.post('/', function(req, res, next) { //I will set this as a POST request so that Angular knows to post my 'message' data to my mongoDb backend.
+    var message = new Message({
+        content: req.body.content
+    });
+    message.save(function(err, result) {
+        if (err) { // Error handler
+            return res.status(500).json({
+                title: 'Dang, it broke!',
+                error: err
+            });
+        }
+        res.status(201).json({ //I am setting a 'everything's ok' code for 201
+            message: 'Message successfully posted!',
+            obj: result
+        });
+    });
 });
 
 module.exports = router;
