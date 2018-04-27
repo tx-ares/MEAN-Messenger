35588f40d28839803aed048b1f9f4be88cf0e1c5
Adan <twinadanz@gmail.com>
2018-04-27 16:39:15 -0500

    adding ngInit to message-input component

--- assets/app/messages/message-input.component.ts
+++ assets/app/messages/message-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component , OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
 
 import { MessageService } from "./message.service";
@@ -10,7 +10,7 @@ import { Message } from "./message.model";
     //providers: [MessageService] //Our component can have 'services' injected into it by passing it here, in the providers property.  Here, I will inject my 'MessageService' service into this component, enabling it for use in the scope of this component only.
 })
 
-export class MessageInputComponent {
+export class MessageInputComponent implements OnInit {
     constructor(private messageService: MessageService) {}
 
     onSubmit(form: NgForm) { //We can pass in our 'input value' as an argument in our onSave function.  Here we want to capture the input.value and do something with it later.
@@ -23,4 +23,10 @@ export class MessageInputComponent {
             );
         form.resetForm();
     };
+
+    ngOnInit() {
+        this.messageService.messageInEditMode.subscribe(
+
+        );
+    }
 }
