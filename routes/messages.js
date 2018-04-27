b2bb4a656365ba47e74c47d6e139c9dbae9dc51b
Adan <twinadanz@gmail.com>
2018-04-27 15:54:26 -0500

    added message fetch success handler

--- routes/messages.js
+++ routes/messages.js
@@ -4,6 +4,22 @@ var router = express.Router();
 
 var Message = require('../models/message');
 
+router.get('/' , function(req, res, next) { //Because of redirect defined in app.router.ts I can just use '/' and I will get defaulted to /messages.
+    Message.find() //mongoDb command to fetch all messages when no additional queries are in my route.
+        .exec(function(err, messages) {
+            if (err) { // Error handler
+                return res.status(500).json({
+                    title: 'Dang, it broke!',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'Messages fetch: SUCCESS',
+                obj: messages
+            })
+        })
+});
+
 router.post('/', function(req, res, next) { //I will set this as a POST request so that Angular knows to post my 'message' data to my mongoDb backend.
     var message = new Message({
         content: req.body.content
@@ -16,7 +32,7 @@ router.post('/', function(req, res, next) { //I will set this as a POST request 
             });
         }
         res.status(201).json({ //I am setting a 'everything's ok' code for 201
-            message: 'Message successfully posted!',
+            message: 'Message post: SUCCESS',
             obj: result
         });
     });
