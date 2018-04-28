8450a0dd5336b2cb2da59e34e391dcedb79abf4c
Adan <twinadanz@gmail.com>
2018-04-28 14:49:42 -0500

    define post route for user

--- routes/user.js
+++ routes/user.js
@@ -1,1 +1,29 @@
 //This will contain user related routes.  So sub-routes or child routes like '/user/profile' or something.
+var express = require('express');
+var router = express.Router();
+var bcrypt = require('bcrypt'); // This is our hash encryption package. It can be used to secure data like a password.
+
+var User = require('../models/user');
+
+router.post('/', function(req, res, next) {
+    var user = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        password: bcrypt.hashSync(req.body.password, 10), // Data like a password NEEDS to be encrypted.  Also sending it over SSL is a good way to address this security issue.
+        email: req.body.email
+    });
+    user.save(function(err, result){
+        if (err) { // Error handler
+            return res.status(500).json({
+                title: 'The was an error creating a User.',
+                error: err
+            });
+        }
+        res.status(201).json({
+            message: 'User create: SUCCESS',
+            obj: result
+        });
+    });
+});
+
+module.exports = router;
