6370c8e7218be04c10c3270409e072e4c66c0b98
Adan <twinadanz@gmail.com>
2018-05-08 23:41:59 -0500

    create headers for signin service

--- assets/app/auth/auth.service.ts
+++ assets/app/auth/auth.service.ts
@@ -17,6 +17,10 @@ export class AuthService {
     };
 
     signIn(user: User) {
-        
+        const body = JSON.stringify(user);
+        const headers = new Headers({'Content-Type': 'application/json'});
+        return this.http.post('http://localhost:3000/user/', body, {headers: headers})
+            .map((response: Response) => response.json())
+            .catch((error: Response) => Observable.throw(error.json()));
     }
 };
