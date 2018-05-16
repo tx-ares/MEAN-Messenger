8fad5f86dd8e305143e8f938ed425d323c61531f
Adan <twinadanz@gmail.com>
2018-05-15 23:19:16 -0500

    add token to requests

--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -14,7 +14,9 @@ export class MessageService {
     addMessage(message: Message ) { //This is our main function of the 'MessageService'
         const body = JSON.stringify(message)
         const headers = new Headers({'Content-Type': 'application/json'});
-        return this.http.post('http://localhost:3000/message', body, {headers: headers}) //This POST request must match the route defined in routes/messages.js.  It does. ;)  Note :  This DOES NOT send the request.  It instead creates an Observable that can be 'subscribed' to .
+        const token = localStorage.getItem('token')
+            ? '?token=' + localStorage.getItem('token')
+            : '';        return this.http.post('http://localhost:3000/message', body, {headers: headers}) //This POST request must match the route defined in routes/messages.js.  It does. ;)  Note :  This DOES NOT send the request.  It instead creates an Observable that can be 'subscribed' to .
             .map((response: Response) => {
                 const result = response.json();  //This is from rxjs/Rx and allows for transforming of data.
                 const message = new Message(result.obj.content, 'Check Testerman', result.obj._id, null);
@@ -47,7 +49,10 @@ export class MessageService {
         this.messages.push(message)
         const body = JSON.stringify(message)
         const headers = new Headers({'Content-Type': 'application/json'});
-        return this.http.patch('http://localhost:3000/message/' + message.messageId, body, {headers: headers}) // This is very similar to the addMessage target route, except that it is also expecting a messageId as defined in the patch route.
+        const token = localStorage.getItem('token')
+            ? '?token=' + localStorage.getItem('token')
+            : '';
+        return this.http.patch('http://localhost:3000/message/' + message.messageId + token, body, {headers: headers}) // This is very similar to the addMessage target route, except that it is also expecting a messageId as defined in the patch route.
             .map((response: Response) => response.json())  //This is from rxjs/Rx and allows for transforming of data.
             .catch((error: Response) => Observable.throw(error.json())); //Error handler
 
@@ -55,7 +60,10 @@ export class MessageService {
 
     deleteMessage(message: Message) {
         this.messages.splice(this.messages.indexOf(message) , 1); //We can use the JavaScript method .splice to remove an index from our array.  In this case, to remove a message from the messages array.
-        return this.http.delete('http://localhost:3000/message/' + message.messageId) // This is very similar to the addMessage target route, except that it is also expecting a messageId as defined in the delete route.
+        const token = localStorage.getItem('token')
+            ? '?token=' + localStorage.getItem('token')
+            : '';
+        return this.http.delete('http://localhost:3000/message/' + message.messageId + token) // This is very similar to the addMessage target route, except that it is also expecting a messageId as defined in the delete route.
             .map((response: Response) => response.json())  //This is from rxjs/Rx and allows for transforming of data.
             .catch((error: Response) => Observable.throw(error.json())); //Error handler
     }
