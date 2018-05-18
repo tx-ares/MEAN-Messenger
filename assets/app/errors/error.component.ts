f03ba0cc6843ab6e0815b54f37427e29a72a0572
Adan <twinadanz@gmail.com>
2018-05-18 17:27:19 -0500

    add backdrop styles

--- assets/app/errors/error.component.ts
+++ assets/app/errors/error.component.ts
@@ -5,6 +5,16 @@ import { Error } from "./error.model";
 @Component({
     selector: 'app-error',
     templateUrl: './error.component.html',
+    styles: [`
+        .backdrop {
+            background-color: rgba(0,0,0,0.6);
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100vh;
+        }
+    `]
 })
 
 export class ErrorComponent {
