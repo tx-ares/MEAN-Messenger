d87054c248f2e826ba576bfeb4c20d52541ad3a2
Adan <twinadanz@gmail.com>
2018-05-18 17:12:35 -0500

    added missing token for delete route

--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -70,7 +70,10 @@ export class MessageService {
 
     deleteMessage(message: Message) {
         this.messages.splice(this.messages.indexOf(message) , 1); //We can use the JavaScript method .splice to remove an index from our array.  In this case, to remove a message from the messages array.
-        return this.http.delete('http://localhost:3000/message/' + message.messageId) // This is very similar to the addMessage target route, except that it is also expecting a messageId as defined in the delete route.
+        const token = localStorage.getItem('token')
+            ? '?token=' + localStorage.getItem('token')
+            : '';
+        return this.http.delete('http://localhost:3000/message/' + message.messageId + token) // This is very similar to the addMessage target route, except that it is also expecting a messageId as defined in the delete route.
             .map((response: Response) => response.json())  //This is from rxjs/Rx and allows for transforming of data.
             .catch((error: Response) => Observable.throw(error.json())); //Error handler
     }
